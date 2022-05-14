019ada60a8971b67241808cc9308d2718719c893
Loïc Calcagno <calcagnoloic93@gmail.com>
2022-05-14 17:08:27 +0200

    modification of js + style

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,13 +10,13 @@ let arr = [];
 let mot = null;
 
 // Fonction permettant de choisir un mot aléatoire dans le dictionnaire
-motAlea = () => {
+let motAlea = () => {
   reponse = dicoMot[Math.round(Math.random() * dicoMot.length)];
 }
 
 
 // Fonction permettant de générer les touches du claviers virtuel
-clavier = () => {
+let clavier = () => {
   let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
     `
       <button
@@ -32,8 +32,7 @@ clavier = () => {
 }
 
 // Fonction adaptant le jeu dans le cas où la lettre choisie est correcte ou non
-//Transfo avec .includes()??
-jeu = (lettreJouee) => {
+let jeu = (lettreJouee) => {
   arr.indexOf(lettreJouee) === -1 ? arr.push(lettreJouee) : null;
   document.getElementById(lettreJouee).setAttribute('disabled', true); //empâche de recliquer sur la lettre
   if (reponse.indexOf(lettreJouee) >= 0) {
@@ -51,7 +50,7 @@ jeu = (lettreJouee) => {
 
 /////// INITIALISATION DU JEU APRES CHOIX ALEATOIRE DU MOT
 // Fonction permettant d'afficher les tirets en fonction du nombre de lettre dans le mot choisi
-tiretMot = () => {
+let tiretMot = () => {
   mot = reponse.split('').map(letter => (arr.indexOf(letter) >= 0 ? letter : " _ ")).join('');
 
   document.getElementById('messageInfo').innerHTML = mot;
@@ -65,34 +64,34 @@ tiretMot();
 
 //////CODE DANS LE CAS OU ON GAGNE
 // Fonction générant un message dans le cas où on gagne
-jeuGagne = () => {
+let jeuGagne = () => {
   if (mot === reponse) {
-    alert("Toutes mes félicitations ")
+    alert("Well done!")
   }
 }
 
 //////CODE DANS LE CAS OU ON PERD
 // Fonction permettant de changer l'image du pendu si on se trompe
-pendu = () => {
+let pendu = () => {
   document.getElementById('pendu').src = './assets/img/' + erreur + '.jpg';
 }
 
 // Fonction mettant à jour le nombre d'erreur
-erreurs = () => {
+let erreurs = () => {
   document.getElementById('erreur').innerHTML = erreur;
 }
 
 // Fonction générant un message dans le cas où on perd
-jeuPerdu = () => {
+let jeuPerdu = () => {
   if (erreur === erreurMax) {
-    alert("Dommage, c'est perdu!")
-    document.getElementById('messageInfo').innerHTML = 'La réponse était : ' + reponse;
+    alert("Oops, it's lose!")
+    document.getElementById('messageInfo').innerHTML = 'The answer was : ' + reponse;
   }
 }
 
 ///// CAS OU LE JOUEUR VEUT REJOUER
 // Fonction permettant de recommencer le jeu 
-reset = () => {
+let reset = () => {
   erreur = 0;
   arr = [];
   document.getElementById('pendu').src = './images/0.jpg';
